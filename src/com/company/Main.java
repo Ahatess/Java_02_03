738bc9e572ed635e837628c53ed564a1c98fa1af
Andrius <andriuscere@gmail.com>
2018-01-30 15:08:56 +0200

    Uzduotis 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Iveskite 2 skaicius");
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	System.out.println("Suma: " + (a+b));
+	System.out.println("Skirtumas: " + (a-b));
     }
 }
